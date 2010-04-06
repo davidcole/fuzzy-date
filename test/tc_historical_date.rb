af3543f18473a77f57c786fbe0d9a9ce8f05104d
David Cole <github@davidcole.net>
2010-04-06 16:30:01 -0400

    Organize test by date format.
    Add YYYY-MM test.

--- test/tc_historical_date.rb
+++ test/tc_historical_date.rb
@@ -7,7 +7,7 @@ require 'historical_date'
 
 class HistoricalDateTest < Test::Unit::TestCase
 
-  def test_dates
+  def test_date_YYYY_MM_DD
     hd = HistoricalDate.parse_date( '2010-04-05' )
     assert_equal( hd[ :fixed ], '2010-04-05' )
     assert_equal( hd[ :month_name ], 'April' )
@@ -21,6 +21,22 @@ class HistoricalDateTest < Test::Unit::TestCase
     assert_equal( hd[ :era ], 'AD' )
     assert_equal( hd[ :long ], 'April 5, 2010 AD' )
     assert_equal( hd[ :original ], '2010-04-05' )
+  end
+
+  def test_date_YYYY_MM
+    hd = HistoricalDate.parse_date( '2010-04' )
+    assert_equal( hd[ :fixed ], '2010-04' )
+    assert_equal( hd[ :month_name ], 'April' )
+    assert_equal( hd[ :circa ], false )
+    assert_equal( hd[ :month ], 4 )
+    assert_nil( hd[ :errors ] )
+    assert_nil( hd[ :day ] )
+    assert_equal( hd[ :year ], 2010 )
+    assert_equal( hd[ :full ], 'April, 2010 AD' )
+    assert_equal( hd[ :short ], '4/2010 AD' )
+    assert_equal( hd[ :era ], 'AD' )
+    assert_equal( hd[ :long ], 'April, 2010 AD' )
+    assert_equal( hd[ :original ], '2010-04' )
   end
 
 end
