0bd5de2bc204a58d3bb75183f4a05ae800e4dadb
David Cole <github@davidcole.net>
2010-04-07 12:13:45 -0400

    Change test for nil :errors to assert_raise.

--- test/tc_historical_date.rb
+++ test/tc_historical_date.rb
@@ -7,66 +7,93 @@ require 'historical_date'
 
 class HistoricalDateTest < Test::Unit::TestCase
 
+  def test_date_gibberish
+    date = 'gibberish'
+    assert_raise( ArgumentError ){ HistoricalDate.parse_date( date ) }
+  end
+
   def test_date_YYYY_MM_DD
-    hd = HistoricalDate.parse_date( '2010-04-05' )
+    date = '2010-04-05'
+    hd = HistoricalDate.parse_date( date )
     assert_equal( '2010-04-05', hd[ :fixed ] )
     assert_equal( 'April', hd[ :month_name ] )
     assert_equal( false, hd[ :circa ] )
     assert_equal( 4, hd[ :month ] )
-    assert_nil( hd[ :errors ] )
+    #assert_nil( hd[ :errors ] )
     assert_equal( 5, hd[ :day ] )
     assert_equal( 2010, hd[ :year ] )
     assert_equal( 'Monday, April 5, 2010 AD', hd[ :full ] )
     assert_equal( '4/5/2010 AD', hd[ :short ] )
     assert_equal( 'AD', hd[ :era ] )
     assert_equal( 'April 5, 2010 AD', hd[ :long ] )
-    assert_equal( '2010-04-05', hd[ :original ] )
+    assert_equal( date, hd[ :original ] )
+    
+    date = '2010 04 05 BC'
+    hd = HistoricalDate.parse_date( date )
+    assert_equal( '2010-04-05', hd[ :fixed ] )
+    assert_equal( 'April', hd[ :month_name ] )
+    assert_equal( false, hd[ :circa ] )
+    assert_equal( 4, hd[ :month ] )
+    #assert_nil( hd[ :errors ] )
+    assert_equal( 5, hd[ :day ] )
+    assert_equal( 2010, hd[ :year ] )
+    assert_equal( 'Tuesday, April 5, 2010 BC', hd[ :full ] )
+    assert_equal( '4/5/2010 BC', hd[ :short ] )
+    assert_equal( 'BC', hd[ :era ] )
+    assert_equal( 'April 5, 2010 BC', hd[ :long ] )
+    assert_equal( date, hd[ :original ] )
   end
 
   def test_date_YYYY_MM
-    hd = HistoricalDate.parse_date( '2010-04' )
+    date = '2010-04'
+    hd = HistoricalDate.parse_date( date )
     assert_equal( '2010-04', hd[ :fixed ] )
     assert_equal( 'April', hd[ :month_name ] )
     assert_equal( false, hd[ :circa ] )
     assert_equal( 4, hd[ :month ] )
-    assert_nil( hd[ :errors ] )
+    #assert_nil( hd[ :errors ] )
     assert_nil( hd[ :day ] )
     assert_equal( 2010, hd[ :year ] )
     assert_equal( 'April, 2010 AD', hd[ :full ] )
     assert_equal( '4/2010 AD', hd[ :short ] )
     assert_equal( 'AD', hd[ :era ] )
     assert_equal( 'April, 2010 AD', hd[ :long ] )
-    assert_equal( '2010-04', hd[ :original ] )
+    assert_equal( date, hd[ :original ] )
+    
+    date = '2010-23'
+    assert_raise( ArgumentError ){ HistoricalDate.parse_date( date ) }
   end
 
   def test_date_YYYY
-    hd = HistoricalDate.parse_date( '2010' )
+    date = '2010'
+    hd = HistoricalDate.parse_date( date )
     assert_equal( '2010', hd[ :fixed ] )
     assert_nil( hd[ :month_name ] )
     assert_equal( false, hd[ :circa ] )
     assert_nil( hd[ :month ] )
-    assert_nil( hd[ :errors ] )
+    #assert_nil( hd[ :errors ] )
     assert_nil( hd[ :day ] )
     assert_equal( 2010, hd[ :year ] )
     assert_equal( '2010 AD', hd[ :full ] )
     assert_equal( '2010 AD', hd[ :short ] )
     assert_equal( 'AD', hd[ :era ] )
     assert_equal( '2010 AD', hd[ :long ] )
-    assert_equal( '2010', hd[ :original ] )
+    assert_equal( date, hd[ :original ] )
     
-    hd = HistoricalDate.parse_date( '2010 BC' )
+    date = '2010 BC'
+    hd = HistoricalDate.parse_date( date )
     assert_equal( '2010', hd[ :fixed ] )
     assert_nil( hd[ :month_name ] )
     assert_equal( false, hd[ :circa ] )
     assert_nil( hd[ :month ] )
-    assert_nil( hd[ :errors ] )
+    #assert_nil( hd[ :errors ] )
     assert_nil( hd[ :day ] )
     assert_equal( 2010, hd[ :year ] )
     assert_equal( '2010 BC', hd[ :full ] )
     assert_equal( '2010 BC', hd[ :short ] )
     assert_equal( 'BC', hd[ :era ] )
     assert_equal( '2010 BC', hd[ :long ] )
-    assert_equal( '2010 BC', hd[ :original ] )
+    assert_equal( date, hd[ :original ] )
   end
 
 end
