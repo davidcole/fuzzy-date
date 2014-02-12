f8d16954c6bb2c2b2f1216691921bcfe04092eae
David Cole <david.cole@digitalcharleston.com>
2014-02-12 07:14:14 -0500

    Readability.

--- lib/historical_date.rb
+++ lib/historical_date.rb
@@ -103,31 +103,31 @@ class HistoricalDate
     return '' if date == ''
 
     date_parts = {}
-    date_parts[:original] = date
-    date_parts[:circa] = false
-    date_parts[:year] = nil
-    date_parts[:month] = nil
-    date_parts[:day] = nil
-    date_parts[:era] = 'AD'
+    date_parts[ :original ] = date
+    date_parts[ :circa    ] = false
+    date_parts[ :year     ] = nil
+    date_parts[ :month    ] = nil
+    date_parts[ :day      ] = nil
+    date_parts[ :era      ] = 'AD'
 
     date_in_parts = []
 
-    date_separator = Regexp.new('[^A-Za-z0-9]', true) # Regexp.new("[- /.,]", true)
+    date_separator = Regexp.new '[^A-Za-z0-9]', true
 
     #- Split the string
 
-    date_in_parts = date.split(date_separator)
+    date_in_parts = date.split date_separator
     date_in_parts.delete_if { |d| d.to_s.empty? }
-    if date_in_parts.first.match(Regexp.new("#{ @circa_words.join('|') }", true)) then
-      date_parts[:circa] = true
+    if date_in_parts.first.match Regexp.new( @circa_words.join( '|' ), true )
+      date_parts[ :circa ] = true
       date_in_parts.shift
     end
-    if date_in_parts.last.match(Regexp.new("#{ @era_words.join('|') }", true)) then
-      date_parts[:era] = date_in_parts.pop.upcase.strip
+    if date_in_parts.last.match Regexp.new( @era_words.join( '|' ), true )
+      date_parts[ :era ] = date_in_parts.pop.upcase.strip
     end
 
-    date = date_in_parts.join('-')
-    date_parts[:fixed] = date
+    date = date_in_parts.join '-'
+    date_parts[ :fixed ] = date
 
     #- Takes care of YYYY
     if date =~ /^(\d{1,4})$/
@@ -160,23 +160,23 @@ class HistoricalDate
       year = $2.to_i.to_s
 
     #- Takes care of DD-MMM-YYYY and DD-MMM
-    elsif date =~ /^(\d{1,2})(?:-(#{ @month_abbreviations.keys.join('|') }).*?(?:-(\d{1,4}))?)?$/i
+    elsif date =~ /^(\d{1,2})(?:-(#{ @month_abbreviations.keys.join( '|' ) }).*?(?:-(\d{1,4}))?)?$/i
       month_text = $2.to_s.capitalize
-      month = @month_names.key(@month_abbreviations[month_text]).to_i.to_s
+      month = @month_names.key( @month_abbreviations[ month_text ] ).to_i.to_s
       day = $1.to_i.to_s
       year = $3 ? $3.to_i.to_s : nil
 
     #- Takes care of MMM-DD-YYYY
-    elsif date =~ /^(#{ @month_abbreviations.keys.join('|') }).*?-(\d{1,2})-(\d{1,4})$/i
+    elsif date =~ /^(#{ @month_abbreviations.keys.join( '|' ) }).*?-(\d{1,2})-(\d{1,4})$/i
       month_text = $1.to_s.capitalize
-      month = @month_names.key(@month_abbreviations[month_text]).to_i.to_s
+      month = @month_names.key( @month_abbreviations[ month_text ] ).to_i.to_s
       day = $2.to_i.to_s
       year = $3 ? $3.to_i.to_s : nil
 
     #- Takes care of MMM-YYYY and MMM
-    elsif date =~ /^(#{ @month_abbreviations.keys.join('|') }).*?(?:-(\d{1,4}))?$/i
+    elsif date =~ /^(#{ @month_abbreviations.keys.join( '|' ) }).*?(?:-(\d{1,4}))?$/i
       month_text = $1.to_s.capitalize
-      month = @month_names.key(@month_abbreviations[month_text]).to_i.to_s
+      month = @month_names.key( @month_abbreviations[ month_text ] ).to_i.to_s
       day = nil
       year = $2 ? $2.to_i.to_s : nil
 
@@ -184,50 +184,50 @@ class HistoricalDate
       raise ArgumentError.new( 'Cannot parse date.' )
     end
 
-    date_parts[:year] = year ? year.to_i : nil
-    date_parts[:month] = month ? month.to_i : nil
-    date_parts[:day] = day ? day.to_i : nil
+    date_parts[ :year   ] = year  ? year.to_i   : nil
+    date_parts[ :month  ] = month ? month.to_i  : nil
+    date_parts[ :day    ] = day   ? day.to_i    : nil
     #return { :circa => "day: #{ day }, month: #{ month }, year: #{ year }" }
 
     #- Some error checking at this point
-    if month.to_i > 13 then
+    if month.to_i > 13
       raise ArgumentError.new( 'Month cannot be greater than 12.' )
-    elsif month and day and day.to_i > @days_in_month[month.to_i] then
-      unless month.to_i == 2 and year and Date.parse('1/1/' + year).leap? and day.to_i == 29 then
+    elsif month and day and day.to_i > @days_in_month[ month.to_i ]
+      unless month.to_i == 2 and year and Date.parse( '1/1/' + year ).leap? and day.to_i == 29
         raise ArgumentError.new( 'Too many days in this month.' )
       end
-    elsif month and month.to_i < 1 then
+    elsif month and month.to_i < 1
       raise ArgumentError.new( 'Month cannot be less than 1.' )
-    elsif day and day.to_i < 1 then
+    elsif day and day.to_i < 1
       raise ArgumentError.new( 'Day cannot be less than 1.' )
     end
 
-    month_name = @month_names[month.to_i]
-    date_parts[:month_name] = month_name
+    month_name = @month_names[ month.to_i ]
+    date_parts[ :month_name ] = month_name
 
     # ----------------------------------------------------------------------
 
-    show_era = ' ' + date_parts[:era]
-    show_circa = date_parts[:circa] == true ? 'About ' : ''
+    show_era = ' ' + date_parts[ :era ]
+    show_circa = date_parts[ :circa ] == true ? 'About ' : ''
 
-    if year and month and day then
-      date_parts[:short] = show_circa + month + '/' + day + '/' + year + show_era
-      date_parts[:long] = show_circa + month_name + ' ' + day + ', ' + year + show_era
+    if year and month and day
+      date_parts[ :short  ] = show_circa + month + '/' + day + '/' + year + show_era
+      date_parts[ :long   ] = show_circa + month_name + ' ' + day + ', ' + year + show_era
       modified_long = show_circa + month_name + ' ' + day + ', ' + year.rjust( 4, "0" ) + show_era
-      date_parts[:full] = show_circa + Date.parse( modified_long ).strftime( '%A,' ) + Date.parse( day + ' ' + month_name + ' ' + year.rjust( 4, "0" ) ).strftime( ' %B %-1d, %Y' ) + show_era
-    elsif year and month then
-      date_parts[:short] = show_circa + month + '/' + year + show_era
-      date_parts[:long] = show_circa + month_name + ', ' + year + show_era
-      date_parts[:full] = date_parts[:long]
-    elsif month and day then
+      date_parts[ :full   ] = show_circa + Date.parse( modified_long ).strftime( '%A,' ) + Date.parse( day + ' ' + month_name + ' ' + year.rjust( 4, "0" ) ).strftime( ' %B %-1d, %Y' ) + show_era
+    elsif year and month
+      date_parts[ :short  ] = show_circa + month + '/' + year + show_era
+      date_parts[ :long   ] = show_circa + month_name + ', ' + year + show_era
+      date_parts[ :full   ] = date_parts[ :long ]
+    elsif month and day
       month_text = @month_abbreviations.key(month_text) || month_text
-      date_parts[:short] = show_circa + day + '-' + month_text
-      date_parts[:long] = show_circa + day + ' ' + month_name
-      date_parts[:full] = date_parts[:long]
-    elsif year then
-      date_parts[:short] = show_circa + year + show_era
-      date_parts[:long] = date_parts[:short]
-      date_parts[:full] = date_parts[:long]
+      date_parts[ :short  ] = show_circa + day + '-' + month_text
+      date_parts[ :long   ] = show_circa + day + ' ' + month_name
+      date_parts[ :full   ] = date_parts[ :long ]
+    elsif year
+      date_parts[ :short  ] = show_circa + year + show_era
+      date_parts[ :long   ] = date_parts[ :short  ]
+      date_parts[ :full   ] = date_parts[ :long   ]
     end
 
     return date_parts
