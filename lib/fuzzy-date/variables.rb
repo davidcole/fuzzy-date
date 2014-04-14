4438f1b70359615b49b1296c79c8831d234661a7
David Cole <david.cole@digitalcharleston.com>
2014-04-14 07:54:44 -0400

    Refactor out variables.

---
+++ lib/fuzzy-date/variables.rb
@@ -0,0 +1,77 @@
+module FuzzyDate
+
+  DATE_SEPARATOR = '[^A-Za-z0-9]'
+
+  @month_names = {
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+    }
+
+  @month_abbreviations = {
+    'Jan' => 'January',
+    'Feb' => 'February',
+    'Mar' => 'March',
+    'Apr' => 'April',
+    'May' => 'May',
+    'Jun' => 'June',
+    'Jul' => 'July',
+    'Aug' => 'August',
+    'Sep' => 'September',
+    'Oct' => 'October',
+    'Nov' => 'November',
+    'Dec' => 'December'
+    }
+
+  @days_in_month = {
+    1 => 31,
+    2 => 28,
+    3 => 31,
+    4 => 30,
+    5 => 31,
+    6 => 30,
+    7 => 31,
+    8 => 31,
+    9 => 30,
+    10 => 31,
+    11 => 30,
+    12 => 31
+    }
+
+  @range_words = [
+    'Between',
+    'Bet',
+    'Bet.',
+    'From'
+    ]
+
+  @middle_range_words = [
+    # '-',  -  Not used because it is more commonly used as a delimiter
+    'To',
+    'And'
+    ]
+
+  @circa_words = [
+    'Circa',
+    'About',
+    'Abt',
+    'Abt.',
+    '~'
+    ]
+
+  @era_words = [
+    'AD',
+    'BC',
+    'CE',
+    'BCE'
+    ]
+end
